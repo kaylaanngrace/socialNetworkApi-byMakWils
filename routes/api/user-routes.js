3c7012c02269604f0b3c0892c29db2019ba88962
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-05-19 18:46:25 -0500

    user controller and routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,29 @@
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+  } = require('../../controllers/user-controller');
+
+// GET all and POST at /api/users
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+// GET one, PUT, and DELETE at /api/users/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router
+  .route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(deleteFriend);
+
+module.exports = router;
