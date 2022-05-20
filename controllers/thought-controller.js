661eb4fdcaf9941ea711545dbbdbb6d7dcc5715f
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-05-19 19:07:51 -0500

    GET all thoughts- thought controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,20 @@
+const { User, Thought} = require('../models');
+
+const thoughtController = {
+  // get all thoughts
+  getAllThought(req, res) {
+    Thought.find({})
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+};
